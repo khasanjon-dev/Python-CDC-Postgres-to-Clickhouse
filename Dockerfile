601098dd966e0fa31f14e96a8e6ecd047d820314
Khasanjon <khasanjon.dev@gmail.com>
2025-01-16 18:01:56 +0500

    add new actions

---
+++ Dockerfile
@@ -0,0 +1,8 @@
+FROM debezium/connect:2.7.3.Final
+
+
+# Create a directory inside the container
+RUN mkdir -p /kafka/connect/debezium-avro-converter
+
+# Copy files from the host to the container
+COPY ./debezium-avro-converter /kafka/connect/debezium-avro-converter
--- main.py
+++ main.py
@@ -1,54 +1,70 @@
+import json
+from datetime import datetime
+
 import psycopg2
 from clickhouse_driver import Client
+from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
 
-# PostgreSQL connection details
-pg_conn = psycopg2.connect(
-    dbname="postgres",
-    user="postgres",
-    password="1",
-    host="localhost",
-    port="5432"
-)
+# PostgreSQL connection parameters
+pg_conn_params = {
+    'dbname': 'postgres',
+    'user': 'postgres',
+    'password': '1',
+    'host': 'localhost',
+    'port': '5432'
+}
 
-# ClickHouse connection details
-ch_client = Client(
-    host='localhost',
-    user='default',
-    password='default',
-    database='default'
-)
+# ClickHouse connection parameters
+ch_conn_params = {
+    'host': 'localhost',
+    'port': 9000,
+    'user': 'default',
+    'password': ''
+}
 
+# Connect to PostgreSQL
+pg_conn = psycopg2.connect(**pg_conn_params)
+pg_conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
+pg_cursor = pg_conn.cursor()
 
-def get_schemas_and_tables(pg_conn):
-    cursor = pg_conn.cursor()
-    cursor.execute("""
-        SELECT table_schema, table_name
-        FROM information_schema.tables
-        WHERE table_type = 'BASE TABLE' AND table_schema NOT IN ('information_schema', 'pg_catalog')
-    """)
-    return cursor.fetchall()
+# Connect to ClickHouse
+ch_client = Client(**ch_conn_params)
 
+# Listen for changes in PostgreSQL
+pg_cursor.execute("LISTEN user_change;")
 
-def transfer_table_data(pg_conn, ch_client, schema, table):
-    cursor = pg_conn.cursor()
-    cursor.execute(f'SELECT * FROM {schema}.{table}')
-    columns = [desc[0] for desc in cursor.description]
-    rows = cursor.fetchall()
+print("Waiting for notifications on channel 'user_change'...")
 
-    # Create destination table name with codename_schema
-    ch_table = f'{table}'
+while True:
+    # Wait for notifications
+    pg_conn.poll()
+    while pg_conn.notifies:
+        notify = pg_conn.notifies.pop()
+        payload = json.loads(notify.payload)
+        schema = payload.get('schema')
+        action = payload.get('action')
+        data = payload.get('data')
 
-    # Insert data into ClickHouse
-    for row in rows:
-        ch_client.execute(
-            f'INSERT INTO {ch_table} ({", ".join(columns)}, codename_schema) VALUES',
-            [tuple(row) + (schema,)]
-        )
+        # Format the created_at field to remove fractional seconds
+        created_at = datetime.strptime(data['created_at'], '%Y-%m-%dT%H:%M:%S.%f').strftime('%Y-%m-%d %H:%M:%S')
 
+        # Determine is_deleted value based on action and insert data into ClickHouse
+        is_deleted = 0 if action in ['INSERT', 'UPDATE'] else 1
 
-schemas_and_tables = get_schemas_and_tables(pg_conn)
+        ch_client.execute('''
+            INSERT INTO postgres.users (id, username, email, created_at, codename_schema, is_deleted)
+            VALUES (%(id)s, %(username)s, %(email)s, %(created_at)s, %(codename_schema)s, %(is_deleted)s)
+        ''', {
+            'id': data['id'],
+            'username': data['username'],
+            'email': data['email'],
+            'created_at': created_at,
+            'codename_schema': schema,
+            'is_deleted': is_deleted
+        })
 
-for schema, table in schemas_and_tables:
-    transfer_table_data(pg_conn, ch_client, schema, table)
+        print(f"{action} data into ClickHouse: {data}")
 
+# Close connections
+pg_cursor.close()
 pg_conn.close()
